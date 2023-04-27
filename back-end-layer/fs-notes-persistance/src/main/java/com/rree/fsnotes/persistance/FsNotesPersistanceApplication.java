19508d401f36c218fbbae4be645cd38d59f9aeb9
Robert Elizalde <devrced@outlook.com>
2023-04-26 21:28:21 -0600

    Init commit

---
+++ back-end-layer/fs-notes-persistance/src/main/java/com/rree/fsnotes/persistance/FsNotesPersistanceApplication.java
@@ -0,0 +1,13 @@
+package com.rree.fsnotes.persistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsNotesPersistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsNotesPersistanceApplication.class, args);
+	}
+
+}
---
+++ back-end-layer/fs-notes-persistance/src/test/java/com/rree/fsnotes/persistance/FsNotesPersistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rree.fsnotes.persistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsNotesPersistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
