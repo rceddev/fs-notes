39c9469b0a8dae411d191d77744dac4ef7593c24
Robert Elizalde <devrced@outlook.com>
2024-06-01 22:29:05 -0600

    New - Basic header implemented with angualar material with no behavior

--- front-end-layer/fs-web-app/src/app/app.component.ts
+++ front-end-layer/fs-web-app/src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    MatSlideToggleModule
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+        RouterOutlet,
+        MatSlideToggleModule,
+        RouterLink,
+        HeaderComponent
+    ]
 })
 export class AppComponent {
   title = 'fs-web-app';
