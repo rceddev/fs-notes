4197dd207fcde9de896fb32fa6c376f6013e09f8
Robert Elizalde <devrced@outlook.com>
2024-07-13 14:19:29 -0600

    New - Register logic implementation with all functionality and error handler

--- front-end-layer/fs-web-app/src/app/services/user.service.ts
+++ front-end-layer/fs-web-app/src/app/services/user.service.ts
@@ -8,13 +8,27 @@ export interface UserToken{
   token:string
 }
 
+export interface UserRegistered{
+  user: {firstName: string,lastName: string, id:number,email: string },
+  token: string
+}
+
 export interface UserLogin{
+  password: string,
+  email: string
+}
+
+export interface UserRegister{
+  firstName: string,
+  lastName: string,
   password: string,
   email: string
 }
 
 const WRONG_PASS_CODE = "401_WP";
 const EMAIL_NOT_FOUND_CODE = "401_ENF";
+const FS_BAD_REQUEST = "4XX_BR";
+const USER_ALREADY_EXISTS = "409_UE"
 
 @Injectable({
   providedIn: 'root'
@@ -42,25 +56,60 @@ export class UserService {
       
       catchError((errorResponse)=> {
         this.errorHandler(errorResponse);
-        return throwError(()=> new Error('Algo falló. Por favor intente nuevamente.'));
+        return throwError(()=> new Error('Something wrong happend trying to log in user'));
       })
     );
   }
 
+  register(body: UserRegister): Observable<UserRegistered>{
+    return this.http.post<UserRegistered>(environment.urlHost + environment.pathRegister, body).pipe(
+      tap( (response) => {
+        this.isUserLogedIn.next(true);
+        sessionStorage.setItem('token', response.token);
+        sessionStorage.setItem('loggedOn', 'true');
+        sessionStorage.setItem('firstName', response.user.firstName);
+        sessionStorage.setItem('lastName', response.user.lastName);
+        sessionStorage.setItem('email', response.user.email);
+        sessionStorage.setItem('id', String(response.user.id));
+        //TODO: Check if id its necessary, if not, remove for security porpuse
+      }),
+
+      catchError((errorResponse)=> {
+        this.errorHandler(errorResponse);
+        return throwError(()=> new Error('Something wrong happend trying to register user'));
+      })
+    )
+  }
+
   private errorHandler(errorResponse: HttpErrorResponse){
 
-    if (errorResponse.status == 401) {
-      switch (errorResponse.error.code) {
-        case WRONG_PASS_CODE:
-          console.log(errorResponse.error.message);
-          this.snackBarService.openSnackBar("chingatu madre");
-          break;
-        case EMAIL_NOT_FOUND_CODE:
-          this.snackBarService.openSnackBar(String(errorResponse.error.message));
-          break;
-        default:
-          break;
+
+
+    if (errorResponse.status >= 400 && errorResponse.status <= 499) {
+      if (errorResponse.error.code) {
+        switch (errorResponse.error.code) {
+          case WRONG_PASS_CODE:
+            this.snackBarService.openSnackBar(String(errorResponse.error.message));
+            break;
+          case EMAIL_NOT_FOUND_CODE:
+            this.snackBarService.openSnackBar(String(errorResponse.error.message));
+            break;
+          case FS_BAD_REQUEST:
+            this.snackBarService.openSnackBar(String(errorResponse.error.message));
+            break;
+          case USER_ALREADY_EXISTS:
+            this.snackBarService.openSnackBar(String(errorResponse.error.message));
+            break;
+          default:
+            this.snackBarService.openSnackBar(String("Somthing goes wrong"));
+            break;
+        }
+      }else{
+        //TODO: Error no throw by microservices
       }
+    }
+    if (errorResponse.status >= 500 && errorResponse.status <= 599) {
+      
     }
     
     
